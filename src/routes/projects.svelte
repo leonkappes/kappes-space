fb0288aa90ebe3490184da245420d401ef0298c0
Leon Kappes <meluten@gmail.com>
2022-10-02 17:38:25 +0200

    fix content errors and add new information

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -1,3 +1,7 @@
+<svelte:head>
+	<title>Leon Kappes | Projects</title>
+</svelte:head>
+
 <script>
 import Card from "$lib/Card.svelte";
 
@@ -6,12 +10,13 @@ import Card from "$lib/Card.svelte";
     <div class="flex justify-center align-top flex-col pt-6">
         <h1 class="text-white text-center text-3xl">My Projects</h1>
         <div id="project-rows" class="grid grid-cols-1 md:grid-cols-2 w-full m-auto p-14">
-            <Card title="This website" description="Build with svelte(-kit) and builded as a docker image" href="https://github.com/leonkappes/kappes-space"></Card>
+            <Card title="This website" description="Build with svelte(-kit) and deployed as a docker image" href="https://github.com/leonkappes/kappes-space"></Card>
             <Card title="Research paper | Facharbeit" description="A research paper on websockets (German) | Meine Facharbeit zum Thema Websockets" href="https://github.com/leonkappes/websocket-paper"></Card>
             <Card title="Saltychat Troubleshooter" description="A little tool(winforms) for the saltychat Teamspeak 3 Plugin. It resolves common errors endusers faced" href="https://github.com/leonkappes/Saltytroubleshooter"></Card>
-            <Card title="FXServer Manager" description="Automatically restarts an FXServer/Fivem-Server(servers provided by the community for the game Grand Theft Auto V) at configured times and restarts the server if a crash is detected" href="https://github.com/leonkappes/FXServer-Autorestart"></Card>
-            <Card title="Technikapp" description="Website for managing for event-technicans with an event rooster, a inventory and user managment." href="https://github.com/leonkappes/technikapp"></Card>
+            <Card title="FXServer Manager" description="Automatically restart a FXServer/Fivem-Server(servers provided by the community for the game Grand Theft Auto V) at configured times or when a crash is detected." href="https://github.com/leonkappes/FXServer-Autorestart"></Card>
+            <Card title="Technikapp" description="Webapp for managing event-technicans with an event rooster, a inventory and user managment." href="https://github.com/leonkappes/technikapp"></Card>
             <Card title="Helmrepository" description="Helmrepository for my personal projects" href="https://github.com/leonkappes/helm-charts"></Card>
+            <div class="md:col-span-2"><Card title="Github" description="More projects on my github page" href="https://github.com/leonkappes"></Card></div>
         </div>
     </div>
 </div>
